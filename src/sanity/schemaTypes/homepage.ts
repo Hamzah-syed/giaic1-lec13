1448e527459075a7df0a938b5e2b837b66d13101
hamzah syed <hamzah.syed17@gmail.com>
2024-11-29 11:57:43 +0500

    project init

---
+++ src/sanity/schemaTypes/homepage.ts
@@ -0,0 +1,27 @@
+export default {
+    name: 'homepage',
+    title: 'Homepage',
+    type: 'document',
+    fields: [
+      {
+        name: 'heading',
+        title: 'Heading',
+        type: 'string',
+      },
+      {
+        name: 'paragraph',
+        title: 'Paragraph',
+        type: 'text',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+      },
+      {
+        name: 'buttonText',
+        title: 'Button Text',
+        type: 'string',
+      },
+    ],
+  };
---
+++ src/sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import homepage from './homepage'
+
+
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [homepage],
+}
